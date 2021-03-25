f704696040ec23c14bfe14641f730ba432c63c25
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2021-03-25 16:59:52 +0800

    Update : refactoring button component

--- src/Components/Button/button.jsx
+++ src/Components/Button/button.jsx
@@ -4,7 +4,7 @@ import styles from "../../style/css/button.module.css";
 const Button = (props) => {
 	let color = `${styles.blue}`;
 	if (props.color === "green") color = `${styles.green}`;
-	return <button className={`${styles.defaultbtn} ${color}`}>{props.children}</button>;
+	return <button className={`text-white ${styles.defaultbtn} ${color}`}>{props.children}</button>;
 };
 
 export default Button;

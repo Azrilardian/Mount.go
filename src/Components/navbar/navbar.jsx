47733501ff6d667c6f8ba9541cf5f0e169c00cfe
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2021-03-27 23:40:52 +0800

    Update : refactoring navbar dan menambah hamburger menu

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -3,28 +3,45 @@ import styles from "../../style/css/navbar.module.css";
 
 const Navbar = () => {
 	return (
-		<header>
+		<header className="position-sticky">
 			<div className="container">
-				<nav className={`row ${styles.nav}`}>
-					<div className="col-lg-7 col-md-5 col-sm-4">
-						<h3 className={styles.nav__logo}>
-							<a href="#">Mount.go</a>
+				<nav className={`row justify-content-between align-items-center ${styles.nav}`}>
+					<div className="col-lg-7 col-md-5 col-sm-4 col-8">
+						<h3 className={`m-0 ${styles.nav__logo}`}>
+							<a href="#" className="text-white">
+								Mount.go
+							</a>
 						</h3>
 					</div>
-					<ul className={`col-lg-5 col-md-7 col-sm-8 ${styles.nav__navigations}`}>
+					<ul className={`col-lg-5 col-md-7 col-sm-8 m-0 ${styles.nav__navigations}`}>
 						<li className={styles.nav__navigation}>
-							<a href="#">Home</a>
+							<a href="#" className="text-white">
+								Home
+							</a>
 						</li>
 						<li className={styles.nav__navigation}>
-							<a href="#">Info</a>
+							<a href="#" className="text-white">
+								Info
+							</a>
 						</li>
 						<li className={styles.nav__navigation}>
-							<a href="#">Price</a>
+							<a href="#" className="text-white">
+								Price
+							</a>
 						</li>
 						<li className={styles.nav__navigation}>
-							<a href="#">Contact</a>
+							<a href="#" className="text-white">
+								Contact
+							</a>
 						</li>
 					</ul>
+					<div className={`col-4 d-flex justify-content-end`}>
+						<div className={styles.nav__hamburger}>
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+					</div>
 				</nav>
 			</div>
 		</header>

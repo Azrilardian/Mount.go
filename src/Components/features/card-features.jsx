ff20c3bdeda16d243ccf4485d4bfa7dd8b8a41bc
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2021-04-01 15:17:37 +0800

    Update : menambah fitur intersectionObserver pada features section

--- src/Components/features/card-features.jsx
+++ src/Components/features/card-features.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "../../style/css/card-features.module.css";
+import onScreenEffect from "../../js/onScreenEffect";
 
-const Card = (props) => {
-	const { icon, header, paragraph } = props;
-	return (
-		<>
-			<div className="col-lg-3 col-md-6 col-sm-6 col-12 feature d-flex flex-column justify-content-start">
-				<div className={`d-flex align-items-center mb-2 ${styles.feature__heading}`}>
-					<img src={icon} alt={icon} className={styles.feature__heading__icon} />
-					<h5 className={`my-0 ml-2 ${styles.feature__heading__header}`}>{header}</h5>
+class Card extends Component {
+	constructor(props) {
+		super(props);
+	}
+	componentDidMount() {
+		onScreenEffect(".onscreen-element", styles["onscreen-effect"]);
+	}
+	render() {
+		const { icon, header, paragraph } = this.props;
+		return (
+			<>
+				<div className={`col-lg-3 col-md-6 col-sm-6 col-12 d-flex flex-column justify-content-start ${styles.feature}`}>
+					<div className={`onscreen-element ${styles.margin}`}>
+						<div className={`d-flex align-items-center mb-2 ${styles.feature__heading}`}>
+							<img src={icon} alt={icon} className={styles.feature__heading__icon} />
+							<h5 className={`my-0 ml-2 ${styles.feature__heading__header}`}>{header}</h5>
+						</div>
+						<p className={styles.feature__paragraph}>{paragraph}</p>
+					</div>
 				</div>
-				<p className={styles.feature__paragraph}>{paragraph}</p>
-			</div>
-		</>
-	);
-};
+			</>
+		);
+	}
+}
 
 export default Card;

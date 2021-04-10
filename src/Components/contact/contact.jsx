82460712263f54e18167a8615ac897175e9c5171
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2021-04-10 11:09:12 +0800

    Update : memperbaiki responsivitas contact section di ukuran layar < 425px

--- src/Components/contact/contact.jsx
+++ src/Components/contact/contact.jsx
@@ -15,8 +15,8 @@ class Contact extends Component {
 			<section className={`position-relative onscreen-element ${styles.contact}`}>
 				<div className="container">
 					<Heading backgroundText="CONTACT US" whiteText="CONTACT " greenText="US" questionMark="false" position="right"></Heading>
-					<div className="row justify-content-center">
-						<div className={`col-10 p-4 d-flex flex-column onscreen-element ${styles.form}`}>
+					<div className="row justify-content-center px-3">
+						<div className={`col-lg-10 col-md-10 col-sm-10 col-12 p-4 d-flex flex-column onscreen-element ${styles.form}`}>
 							<h4 className={`mt-2 mx-0 ${styles.form__heading}`}>Contact form</h4>
 							<div className={`row justify-content-between ${styles.form__main}`}>
 								<div className={`col-lg-6 col-md-12 col-sm-12 col-12 ${styles.form__main__left}`}>

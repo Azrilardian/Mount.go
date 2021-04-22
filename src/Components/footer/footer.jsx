b006222b0d07754e16485909d9e5bb592e46a34a
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2021-04-22 20:32:18 +0800

    Update : tidak lagi menggunakan css module pada komponen footer

--- src/Components/footer/footer.jsx
+++ src/Components/footer/footer.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import styles from "../../style/css/footer.module.css";
+import "../../style/css/footer.css";
 
 const Footer = () => {
 	return (
-		<footer className={styles.footer}>
+		<footer className="footer">
 			<div className="container">
-				<div className={`row ${styles.row} d-flex justify-content-between align-items-center`}>
+				<div className="row d-flex justify-content-between align-items-center">
 					<div className="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-12">
-						<p className={`m-0 text-white ${styles.footer__copyright}`}>Copyright Azril ardian | 2020 All rights reserved.</p>
+						<p className="m-0 text-white footer__copyright">Copyright Azril ardian | 2020 All rights reserved.</p>
 					</div>
-					<div className={`col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12 ${styles.footer__socmed}`}>
+					<div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12 footer__socmed">
 						<a href="#" className="text-white mr-4">
 							In
 						</a>
